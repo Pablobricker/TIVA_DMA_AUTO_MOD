965e824d937f1c3b331dce241302b68e003c82ee
Pablobricker <pablo_selectrick@comunidad.unam.mx>
2023-08-07 23:10:14 -0600

    comentarios info

--- DMASoftware.h
+++ DMASoftware.h
@@ -1,100 +1,84 @@
 // DMASoftware.c
-// Runs on LM4F120/TM4C123
-// Software triggered memory block transfer
-// Jonathan Valvano
-// September 11, 2013
-
-/* This example accompanies the book
-   "Embedded Systems: Real Time Operating Systems for ARM Cortex M Microcontrollers",
-   ISBN: 978-1466468863, Jonathan Valvano, copyright (c) 2014
-   Section 6.4.5, Program 6.1
-
- Copyright 2014 by Jonathan W. Valvano, valvano@mail.utexas.edu
-    You may use, edit, run or distribute this file
-    as long as the above copyright notice remains
- THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
- OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
- VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
- OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
- For more information about my classes, my research, and my books, see
- http://users.ece.utexas.edu/~valvano/
- */
+// Runs on TM4C1294
 #include <stdint.h>
 #include "tm4c1294ncpdt.h"
 
 
-// The control table used by the uDMA controller.  This table must be aligned to a 1024 byte boundary.
-// each channel has source,destination,control,pad (pad word is ignored)
+// Tabla de control para el microcontrolado uDMA. Esta tabla debe contenee 128 espacios alineados en localidades de 32-bits.
+// cada canal tiene un espacio para la direccion de origen, destino, el control de palabra y un espacio reservado.
 uint32_t ucControlTable[256] __attribute__ ((aligned(1024)));
-// channel 30 is at indices 120,121,122 (primary source,destination,control) and
-//               at indices 248,249,250 (alternate source,destination,control not used)
+// Las localidades correspondientes al canal 30, usado para generar solicitudes por medio de software son
+// 120, 121 y 122 para el origen, desnino y el DMACHCTL respectivamente
 #define CH30 (30*4)
 #define BIT30 0x40000000
 // ************DMA_Init*****************
-// Initialize the memory to memory transfer
-// This needs to be called once before requesting a transfer
-// Inputs:  none
-// Outputs: none
+// Inicializa el controlador uDMA para realizar transferenicas de memoria Flash a memoria RAM
+// Necesita ser llamada antes de realizar la solicitud por software
+// Inputs:  ninguna
+// Outputs: ninguna
 void DMA_Init(void){  int i;
   volatile uint32_t delay; 
   for(i=0; i<256; i++){
     ucControlTable[i] = 0;
   }
-  SYSCTL_RCGCDMA_R = 0x01;    // µDMA Module Run Mode Clock Gating Control
-  delay = SYSCTL_RCGCDMA_R;   // allow time to finish 
-  UDMA_CFG_R = 0x01;          // MASTEN Controller Master Enable
-  UDMA_CTLBASE_R = (uint32_t)ucControlTable;
-  //default Chanel30->DMACHMAP3 is 0x00 Encoding0x00 = software
-  UDMA_PRIOCLR_R = BIT30;     // default, not high priority
-  UDMA_ALTCLR_R = BIT30;      // use primary control
-  UDMA_USEBURSTCLR_R = BIT30; // responds to both burst and single requests
-  UDMA_REQMASKCLR_R = BIT30;  // allow the µDMA controller to recognize requests for this channel
+  SYSCTL_RCGCDMA_R = 0x01;    //0) Reloj al modulo uDMA 1
+  delay = SYSCTL_RCGCDMA_R;   //retardo para que termine de establecerse
+  UDMA_CFG_R = 0x01;          //1) Habilitar operacion de DMA
+  UDMA_CTLBASE_R = (uint32_t)ucControlTable;  //2) Ubicar la tabla de control para los canales
+  //3) default Encoding is 0x00 (software request) for Chanel30->DMACHMAP3 Register
+  UDMA_PRIOCLR_R = BIT30;     //4) No hay prioridades
+  UDMA_ALTCLR_R = BIT30;      //5) Canal 30  usa la estructura de control primaria
+  UDMA_USEBURSTCLR_R = BIT30; //6) Canal 30 responde a solicitudes individuales y en rafagas
+  UDMA_REQMASKCLR_R = BIT30;  //7) Deshabilita al dma a responder solicitudes del canal  30
 }
 
 
 //*************Flash_Init*****************
+//Inicializa el controlador periferico Flash para realizar transferencias 
+//con direccion de origen en memoria Flash
 void Flash_Init(void){
 
-    while((FLASH_PP_R & 0x10000000)== 0);
-    FLASH_DMASZ_R |= 0X01; //2KB = 2*(SZ +1) KB
-    FLASH_DMAST_R = 0X00000020;
+    while((FLASH_PP_R & 0x10000000)== 0);   //0) Revisa si el controlador periferico de flash permite transferencias de datos
+    FLASH_DMASZ_R |= 0X01;                  //1) Tamano maximo de lectura de flash en (kB) 2KB = 2*(SZ +1) KB 
+    FLASH_DMAST_R = 0X00000020;             //2) Direccion de inicio de donde tiene acceso el uDMA para leer
 }
 // ************DMA_Transfer*****************
-// Called to transfer 32-bit words from source to destination
-// Inputs:  source is a pointer to the first 32-bit word of the original data
-//          destination is a pointer to a place to put the copy
-//          count is the number of words to transfer (max is 1024 words)
-// Outputs: none
-// This routine does not wait for completion
+// configura las direcciones de origen y destino de las transferencias solicitadas
+// transferencias de 32 bits individuales
+// La direccion del origen se incrementa en 32 bits, al igual que la direccion destino
+// Entradas: source Apuntador de un buffer en RAM que contiene valores de 0 a 255 o direccion en Flash
+//          destination Apuntador de un buffer en RAM donde se almacena la copia
+//          count numero de palabras a transferir (max 1024 palabras)
+// Esta rutina no espera la finalizacion de la transferencia
+//Se deben haber habilitado todos los modulos antes (uDMA)
+
 void DMA_Transfer(uint32_t *source, uint32_t *destination, uint32_t count){ 
-  ucControlTable[CH30]   = (uint32_t)source+count*4-1;       // last address
-  ucControlTable[CH30+1] = (uint32_t)destination+count*4-1;  // last address
+  ucControlTable[CH30]   = (uint32_t)source+count*4-1;       // direccion origen
+  ucControlTable[CH30+1] = (uint32_t)destination+count*4-1;  // direccion destino
   ucControlTable[CH30+2] = 0xAA00C002+((count-1)<<4);             // DMA Channel Control Word (DMACHCTL)
 /* DMACHCTL          Bits    Value Description
-   DSTINC            31:30   10    32-bit destination address increment
-   DSTSIZE           29:28   10    32-bit destination data size
-   SRCINC            27:26   10    32-bit source address increment
-   SRCSIZE           25:24   10    32-bit source data size
+   DSTINC            31:30   10    32-bit incremento en la direccion destino
+   DSTSIZE           29:28   10    32-bit tamaÃ±o de dato en la localidad destino
+   SRCINC            27:26   10    32-bit incremento en la direccion origen
+   SRCSIZE           25:24   10    32-bit tamaÃ±o de dato en la localidad origen
    reserved          23:18   0     Reserved  
-   ARBSIZE           17:14   0011  Arbitrates after 8 transfers
-   XFERSIZE          13:4  count-1 Transfer count items
-   NXTUSEBURST       3       0     N/A for this transfer type
-   XFERMODE          2:0     010   Use Auto-request transfer mode
+   ARBSIZE           17:14   0011  Arbitrajecada 8 transferencias
+   XFERSIZE          13:4  count-1 conteo de transferencias realizadas
+   NXTUSEBURST       3       0     N/A para este tipo de transferencia
+   XFERMODE          2:0     010   modo Auto-solicitud
   */
-  UDMA_ENASET_R = BIT30;  // µDMA Channel 30 is enabled.
-  UDMA_SWREQ_R = BIT30;   // software start, 
-  // bit 30 in UDMA_ENASET_R become clear when done
-  // bits 2:0 ucControlTable[CH30+2] become clear when done
-  // vector 62, NVIC interrupt 46, vector address 0x0000.00F8 could be armed or µDMA Software interrupt
+  UDMA_ENASET_R = BIT30;  // canal 30 del uDMA habilitado
+  UDMA_SWREQ_R = BIT30;   // instruccion de solicitud por software  
+  // bit 30 en UDMA_ENASET_R se hace cero cuando la transferencia termina
+  // bits 2:0 ucControlTable[CH30+2] se hacen cero cuando la transferencia termina
+  //  existe la interrupcion 44 en el vector 60 del NVIC (0x0000.00F0) que permite 
+  //una rutina de interrupcion cuando la transferencia por software termina
 }
 
 // ************DMA_Status*****************
-// Can be used to check the status of a previous request
-// Inputs:  none
-// Outputs: true if still active, false if complete
-// This routine does not wait for completion
+//Indica el estado de la transferencia
 uint32_t DMA_Status(void){ 
-  return (UDMA_ENASET_R&BIT30);  // µDMA Channel 30 enable nit is high if active
+  return (UDMA_ENASET_R&BIT30);  // 1 si la transferencia del canal 30 sigue activa
+                                 // 0 si ha terminado o no ha epezado
 }
 
--- DMAtestmain.c
+++ DMAtestmain.c
@@ -27,45 +27,40 @@
 
 //*****************************************************************************
 //
-// Blink the on-board LED.
+// Enciende un LED de la tarjeta.
 //
 //*****************************************************************************
-#define PF1       (*((volatile uint32_t *)0x40025008))
-#define PF2       (*((volatile uint32_t *)0x40025010))
-#define PF3       (*((volatile uint32_t *)0x40025020))
-#define LEDS      (*((volatile uint32_t *)0x40025038))
-#define RED       0x02
-#define BLUE      0x04
-#define GREEN     0x08
-const int32_t COLORWHEEL[8] = {RED, RED+GREEN, GREEN, GREEN+BLUE, BLUE, BLUE+RED, RED+GREEN+BLUE, 0};
+
 #define SIZE 128
-//#define SIZE 10
-uint32_t SrcBuf[SIZE],DestBuf[SIZE];
+//Localidades de destino y origen para transferencias por software
+uint32_t SrcBuf = 0x30;   //Flash
+//uint32_t SrcBuf[SIZE];  //RAM
+uint32_t DestBuf[SIZE];
 int main(void){  volatile uint32_t delay; uint32_t i,t;
-  //PLL_Init();  // now running at 80 MHz
-  SYSCTL_RCGCGPIO_R |= 0x20; // enable Port F clock
-  delay = SYSCTL_RCGCGPIO_R;              // allow time to finish 
-  GPIO_PORTF_DIR_R |= 0x01;    // make PF0 output (PF0,F4 built-in LEDs)
-  GPIO_PORTF_AFSEL_R &= ~0x01; // disable alt funct on PF0
-  GPIO_PORTF_DEN_R |= 0x01;    // enable digital I/O on PF0
+  //Configura el puerto F como GPIO para visualizar salida LED
+  SYSCTL_RCGCGPIO_R |= 0x20; // 0) Reloj
+  delay = SYSCTL_RCGCGPIO_R;              // retardo de activacion
+  GPIO_PORTF_DIR_R |= 0x01;    // PF0 como salida
+  GPIO_PORTF_AFSEL_R &= ~0x01; // deshabilita la funcion alterna de PF0
+  GPIO_PORTF_DEN_R |= 0x01;    // modo digital en PF0
                                // configure PF0,F4 as GPIO
   GPIO_PORTF_PCTL_R = (GPIO_PORTF_PCTL_R&0xFFF0FFF0)+0x00000000;
-  GPIO_PORTF_AMSEL_R = 0;      // disable analog functionality on PF
-  //LEDS = 0;
-  GPIO_PORTF_DATA_R = 0;        // turn all LED off
-  DMA_Init();  // initialize DMA channel 30 for software transfer
-  Flash_Init();
+  GPIO_PORTF_AMSEL_R = 0;      // deshabilita la funcion analógica en PF0
+  GPIO_PORTF_DATA_R = 0;        // apaga LED
+  DMA_Init();  // inicializa uDMA para una solicitud de transferencia por sofwr
+  
+  Flash_Init();// inicializa controlador Flash para transferencias de lectura por softwr //Comentar para transferencias sram- sram
   t = 0;
 
   for(i=0;i<SIZE;i++){
-        SrcBuf[i] = i;
+        //SrcBuf[i] = i; //Descomental para transferencias ram - ram
         DestBuf[i] = 0;
       }
-//  while(DMA_Status()); // wait for idle
-//      DMA_Transfer(SrcBuf,DestBuf,SIZE);
 
-  while(DMA_Status()); // wait for idle
-      DMA_Transfer(0x30,DestBuf,SIZE);
+
+  //
+  while(DMA_Status()); // espera a que termine la transferencia
+      DMA_Transfer(SrcBuf,DestBuf,SIZE); 
 
       //TRansferencias de flash comprobadas xd
       GPIO_PORTF_DATA_R = 0X01;
